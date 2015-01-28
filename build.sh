1bbf9bcfbd1e87f7e250333a846d74d7b115e52c
Mijo Repusic <mijo@repusic.com>
2015-01-28 14:41:36 +0100

    Updated Nexus to 2.11.1-01

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build -rm -t griff/sonatype-nexus .
-docker tag griff/sonatype-nexus griff/sonatype-nexus:2.6.4
+docker tag griff/sonatype-nexus griff/sonatype-nexus:2.11.1-01
 docker build -rm -t griff/sonatype-nexus-crowd crowd-plugin
 docker tag griff/sonatype-nexus-crowd griff/sonatype-nexus-crowd:2.6.4-2.0.7
